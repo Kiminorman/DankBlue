fe61be375a7d9a26ed6c16e992010f7676d17882
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2015-04-19 20:43:32 +0300

    en minäkään

--- Reversi/run_cmd.sh
+++ Reversi/run_cmd.sh
@@ -1,4 +1,4 @@
 # !/local/bin/bash
 # host=`hostname`
 # export DISPLAY="${host}:0"
-java -cp .:Reversi_cmd.jar reversi.CommandLineGame $1 $2 $3
+java -cp ../bin:.:Reversi_cmd.jar reversi.CommandLineGame $1 $2 $3
